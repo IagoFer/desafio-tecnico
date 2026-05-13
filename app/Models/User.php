1f21e1c1e597734293f3a87c631240e157e12baa
Iago Fernandes Bastos <iagofernandesbastos@gmail.com>
2026-05-13 13:56:55 -0300

    fix: implementa FilamentUser para habilitar acesso em producao

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -62,5 +65,13 @@ class User extends Authenticatable
     public function endereco()
     {
         return $this->hasOne(Endereco::class);
+    }
+
+    /**
+     * Determine if the user can access the Filament panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email === 'admin@admin.com';
     }
 }
